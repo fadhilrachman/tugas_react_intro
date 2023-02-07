b21500194d66ddf5d31afbe050001e4eb86b266a
fadhilrachman <99377200+fadhilrachman@users.noreply.github.com>
2023-02-07 14:27:51 +0700

    fix deploy

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
   return (
     <div className="App">
       <div>
+        {""}
         <div className="increment">
           ini increment <Increment onClick={handleIncrement} />
         </div>
