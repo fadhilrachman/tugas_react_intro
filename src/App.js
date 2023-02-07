2bdf980f29ef0cf8337861fb00f4bb60e43e2542
fadhilrachman <99377200+fadhilrachman@users.noreply.github.com>
2023-02-07 13:38:19 +0700

    tugas component react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Increment from "./components/Increment";
+import Decrement from "./components/Decrement";
+import React from "react";
 
 function App() {
+  const [value, setValue] = React.useState(0);
+  const handleIncrement = () => {
+    setValue(value + 1);
+  };
+  const handleDeccrement = () => {
+    setValue(value - 1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div className="increment">
+          ini increment <Increment onClick={handleIncrement} />
+        </div>
+        <div className="increment">
+          ini decrement <Decrement onClick={handleDeccrement} />
+        </div>
+      </div>
+      <h1>value = {value}</h1>
     </div>
   );
 }
---
+++ src/components/Increment.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Increment = (props) => {
+  return (
+    <div>
+      <button {...props}>+</button>
+    </div>
+  );
+};
+
+export default Increment;
